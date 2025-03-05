9cb5d27957b6288c2d628f03a26f5d745f895bfc
Rayanna Christensen <christensenrayanna@gmail.com>
2025-03-05 03:44:30 -0300

    config

--- vite.config.ts
+++ vite.config.ts
@@ -5,15 +5,14 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-
+  base: '/RedAccentGallery-Vite/', // Adicione esta linha
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === 'development' && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
